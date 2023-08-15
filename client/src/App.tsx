34b7581fcde1fe7dac17962c4065e7dd59662f3a
Juan David Lasso <juandavid.lasso@dialapplet.com>
2023-08-15 10:40:46 -0500

    commit #7

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom'
+import SignIn from './modules/user/SignIn';
+import SignUp from './modules/user/SignUp';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      Fitco
-    </div>
-  );
+	
+	return (
+		<div className="w-full">
+			<Routes>
+				<Route path='/signin' element={<SignIn />} />
+				<Route path='/signup' element={<SignUp />} />
+				<Route path='*' element={<Navigate to='/signin' replace />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/modules/user/SignIn.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
+
+const SignIn: React.FC = () => {
+
+	const [user, setUser] = useState({
+		email: '',
+		password: ''
+	})
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUser({
+			...user,
+			[e.target.name]: e.target.value
+		})
+	}
+
+	const { email, password} = user
+
+	return (
+		<div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-lg">
+                <h1 className="text-3xl font-semibold text-center text-blue-800">
+                   Sign In
+                </h1>
+                <form className="mt-6">
+                    <div className="mb-2">
+                        <label
+                            htmlFor="email"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Email
+                        </label>
+                        <input
+							value={email}
+							name='email'
+							onChange={handleChange}
+                            type="email"
+                            className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            htmlFor="password"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Password
+                        </label>
+                        <input
+							value={password}
+							name='password'
+							onChange={handleChange}
+                            type="password"
+                            className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+                    <div className="mt-6">
+                        <button
+							className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-700 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+                            Login
+                        </button>
+                    </div>
+                </form>
+
+                <p className="mt-8 text-xs font-light text-center text-gray-700">
+                    {" "}
+                    Don't have an account?{" "}
+                    <Link
+                        to="/signup"
+                        className="font-medium text-blue-600 hover:underline"
+                    >
+                        Sign up
+                    </Link>
+                </p>
+            </div>
+        </div>
+	);
+}
+ 
+export default SignIn;
